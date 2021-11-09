2e81313cfc3ca578dbb37114f205aeac44e3a31c
Emreman5 <emre15087@gmail.com>
2021-11-10 00:07:08 +0300

    ...

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entity.Concrete;
 using Entity.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,12 +23,11 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length<=2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+           
+         
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
