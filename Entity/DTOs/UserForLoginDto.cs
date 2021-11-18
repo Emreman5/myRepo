25fd691caa25ab1af1166f9b71249bb1ef9c7f81
Emreman5 <emre15087@gmail.com>
2021-11-19 01:08:12 +0300

    User login and register dtos

---
+++ Entity/DTOs/UserForLoginDto.cs
@@ -0,0 +1,11 @@
+﻿using Core;
+
+namespace Entity.DTOs
+{
+    public class UserForLoginDto:IDto
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+    
+}
