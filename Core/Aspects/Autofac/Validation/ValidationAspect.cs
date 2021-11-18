717ad56e39026e2f88d6a6fb60975c9c0fbded39
Emreman5 <emre15087@gmail.com>
2021-11-19 01:07:37 +0300

    Security and Extensions added

--- Core/Aspects/Autofac/Validation/ValidationAspect.cs
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -16,7 +16,7 @@ namespace Core.Aspects.Autofac.Validation
         {
             if (!typeof(IValidator).IsAssignableFrom(validatorType))
             {
-                throw new System.Exception("bu değil ");
+                throw new System.Exception("It's not a validator. ");
             }
 
             _validatorType = validatorType;
---
+++ Core/Utilities/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Core.Entities.Concrete;
+
+namespace Core.Utilities.Security.JWT
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+
+
+
+    }
+}
